ffc4c9b3e070a8f3ca34a14199ef30ceaadf4034
Dorian Brown <brown.3794@gmail.com>
2022-02-03 14:51:22 -0500

    feat: :sparkles: Add navbar component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,10 @@
 <script>
 	import Footer from '$lib/components/footer.svelte';
-	// import Header from '$lib/components/Header';
+	import Navbar from '$lib/components/navbar.svelte';
 	import '../app.css';
 </script>
 
+<Navbar />
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
