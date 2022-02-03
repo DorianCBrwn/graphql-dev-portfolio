c6175946816a160c7006fc37175f08136050e546
Dorian Brown <brown.3794@gmail.com>
2022-02-03 13:09:17 -0500

    feat: :sparkles: Add footer component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,10 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
+	// import Header from '$lib/components/Header';
 	import '../app.css';
 </script>
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
+<Footer />
