65d9184c9a479032b0a01b9665fed280925023b0
Dorian Brown <brown.3794@gmail.com>
2022-02-03 17:30:26 -0500

    feat: :sparkles: Add theme select to navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,14 @@
 <script>
 	import Footer from '$lib/components/footer.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
 	import '../app.css';
+
+	//onMount runs the code once the page is visible in teh broswer
+	onMount(async () => {
+		themeChange(false);
+	});
 </script>
 
 <Navbar />
